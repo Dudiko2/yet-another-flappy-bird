54c9dd6612712677763216ce146642b6af92f053
Dudi <dudiko2@gmail.com>
2020-08-14 12:01:53 +0300

    refactor & gitignore

---
+++ src/game.js
@@ -0,0 +1,69 @@
+import img from "./assets/sprite.png";
+import Ground from "./foreground";
+import BG from "./background";
+import Bird from "./bird";
+import Pipes from "./pipes";
+
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+const sprite = document.createElement("img");
+sprite.src = img;
+
+canvas.width = 320;
+canvas.height = 640;
+
+const speed = 120;
+
+const ground = Ground(canvas, ctx, sprite);
+const bg = BG(canvas, ctx, sprite);
+const bird = Bird(canvas, ctx, sprite);
+const createPipes = () => Pipes(canvas, ctx, sprite, ground.h);
+let totalPipes = [createPipes()];
+
+console.log(ground, bg, bird, totalPipes);
+
+const clearCanvas = () => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+const detectCollisions = () => {
+	if (bird.y + bird.h >= ground.y && bird.y <= ground.y + ground.h)
+		bird.ground();
+
+	totalPipes.forEach((p) => {
+		if (bird.isColliding(p) || bird.isColliding({ ...p, y: p.y + p.h + p.gap }))
+			bird.die();
+	});
+};
+
+const update = (secs) => {
+	if (bird.alive) {
+		ground.update(secs, speed);
+		totalPipes.forEach((p) => p.update(secs, speed));
+	}
+	bird.update(secs);
+};
+
+const draw = () => {
+	ctx.fillStyle = "#2960E1";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	bg.draw();
+	totalPipes.forEach((p) => p.draw());
+	ground.draw();
+	bird.draw();
+};
+
+const objectControl = () => {
+	if (totalPipes[totalPipes.length - 1].x <= canvas.width / 2 && bird.alive) {
+		totalPipes.push(createPipes());
+	}
+
+	totalPipes = totalPipes.filter((p) => p.x + p.w > 0);
+};
+
+addEventListener("click", () => {
+	bird.flap();
+});
+
+export { clearCanvas, detectCollisions, update, draw, objectControl };
--- src/script.js
+++ src/script.js
@@ -1,71 +1,19 @@
 import "./style.css";
-import img from "./assets/sprite.png";
-import Ground from "./foreground";
-import BG from "./background";
-import Bird from "./bird";
-import Pipes from "./pipes";
-
-// GLOBALS
-const canvas = document.querySelector("#canvas");
-const ctx = canvas.getContext("2d");
-const sprite = document.createElement("img");
-sprite.src = img;
-
-canvas.width = 320;
-canvas.height = 640;
+import {
+	clearCanvas,
+	detectCollisions,
+	update,
+	draw,
+	objectControl,
+} from "./game";
 
 let prevTime = 0;
-let lastPipeCreated = 0;
-const speed = 120;
-
-// ENTITIES
-const ground = Ground(canvas, ctx, sprite);
-const bg = BG(canvas, ctx, sprite);
-const bird = Bird(canvas, ctx, sprite);
-const createPipes = () => Pipes(canvas, ctx, sprite, ground.h);
-let totalPipes = [createPipes()];
-
-const clearCanvas = () => {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-};
-
-const detectCollisions = () => {
-	if (bird.y + bird.h >= ground.y && bird.y <= ground.y + ground.h)
-		bird.ground();
-
-	totalPipes.forEach((p) => {
-		if (bird.isColliding(p) || bird.isColliding({ ...p, y: p.y + p.h + p.gap }))
-			bird.die();
-	});
-};
-
-const update = (secs) => {
-	if (bird.alive) {
-		ground.update(secs, speed);
-		totalPipes.forEach((p) => p.update(secs, speed));
-	}
-	bird.update(secs);
-};
-
-const draw = () => {
-	ctx.fillStyle = "#2960E1";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-	bg.draw();
-	totalPipes.forEach((p) => p.draw());
-	ground.draw();
-	bird.draw();
-};
 
 const gameLoop = (timestamp) => {
 	const secondsPassed = (timestamp - prevTime) / 1000 || 0;
 	prevTime = timestamp;
 
-	if (totalPipes[totalPipes.length - 1].x <= canvas.width / 2 && bird.alive) {
-		totalPipes.push(createPipes());
-	}
-
-	totalPipes = totalPipes.filter((p) => p.x + p.w > 0);
+	objectControl();
 
 	clearCanvas();
 
@@ -75,9 +23,5 @@ const gameLoop = (timestamp) => {
 
 	requestAnimationFrame(gameLoop);
 };
-
-addEventListener("click", () => {
-	bird.flap();
-});
 
 gameLoop();
